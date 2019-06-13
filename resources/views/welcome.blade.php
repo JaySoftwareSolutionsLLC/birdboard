90de72a1b02c36ac6b6026cb857607e73eb99ad3
JaySoftwareSolutionsLLC <Brettbrewster@jaysoftwaresolutions.com>
2019-06-12 21:50:40 -0400

    O.G. Push

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - BirdBoard
                 </div>
 
                 <div class="links">
